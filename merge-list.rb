71a8f0763d2c0da866549d3c88c3994227ccbd1d
Monica <mntamaru@gmail.com>
2019-07-12 17:35:00 -0700

    second shot - reduce Big O

--- merge-list.rb
+++ merge-list.rb
@@ -28,3 +28,39 @@ end
 puts "First shot: #{merge_arrays([3, 4, 6, 10, 11, 15], [1, 5, 8, 12, 14, 19])}"
 
 
+def merge2 (mine, alice)
+
+	merged = []
+	my_pointer = mine[0]
+	alice_pointer = alice[0]
+
+	count = (mine + alice).length
+
+	count.times do 
+		if my_pointer<alice_pointer
+			merged<< my_pointer
+			mine.shift
+			if mine.length > 0 
+				my_pointer = mine[0]
+			else
+				my_pointer = 1000000
+			end
+
+		else
+			merged << alice_pointer
+			alice.shift
+			if alice.length > 0 
+				alice_pointer = alice[0]
+			else
+				alice_pointer = 1000000
+			end
+		end
+	end
+
+	return merged
+
+end
+
+
+puts "Second shot: #{merge2([3, 4, 6, 10, 11, 15], [1, 5, 8, 12, 14, 19])}"
+
